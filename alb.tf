795d48235a96aaad953d8f28e1b23ea6cc250f37
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-07-03 22:00:39 +0800

    feat: add target group attachment

--- alb.tf
+++ alb.tf
@@ -27,6 +27,18 @@ resource "aws_lb_target_group" "ggz_api" {
   protocol    = "HTTP"
   vpc_id      = "${var.aws_vpc_id}"
   target_type = "instance"
+
+  health_check = {
+    interval = 40
+    path     = "/healthy"
+    matcher  = "200-299"
+  }
+}
+
+resource "aws_lb_target_group_attachment" "ggz_api" {
+  target_group_arn = "${aws_lb_target_group.ggz_api.arn}"
+  target_id        = "${aws_instance.ggz.id}"
+  port             = 80
 }
 
 resource "aws_lb_listener" "front_end" {
