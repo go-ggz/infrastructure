52411875f8d0e8dabe5349080c0c92afc1d4230a
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-07-03 23:57:56 +0800

    feat: Add output

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,24 @@
-output "aws_ggz_eip" {
+output "ggz_public_ip" {
   description = "The IP of the ggz api server"
   value       = "${ aws_eip.ggz_ip.public_ip }"
+}
+
+output "ggz_instance_id" {
+  description = "The ID of the ggz instance"
+  value       = "${ aws_instance.ggz.id }"
+}
+
+output "ggz_alb_sg_id" {
+  description = "The ID of the alb security group"
+  value       = "${ aws_security_group.ggz_alb_sg.id }"
+}
+
+output "ggz_api_sg_id" {
+  description = "The ID of the api security group"
+  value       = "${ aws_security_group.ggz_api_sg.id }"
+}
+
+output "ggz_alb_dns" {
+  description = "The DNS Name of the alb"
+  value       = "${ aws_lb.ggz_api.dns_name }"
 }
