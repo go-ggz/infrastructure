743aaa47be717d44f3a637e3faec0d820d9c5b9b
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-07-07 00:35:24 +0800

    add testing.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

---
+++ terraform_test.go
@@ -0,0 +1,29 @@
+package test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/gruntwork-io/terratest/modules/http-helper"
+	"github.com/gruntwork-io/terratest/modules/terraform"
+)
+
+func TestWebServer(t *testing.T) {
+	terraformOptions := &terraform.Options{
+		// The path to where your Terraform code is located
+		TerraformDir: "./",
+	}
+	// At the end of the test, run `terraform destroy`
+	defer terraform.Destroy(t, terraformOptions)
+	// Run `terraform init` and `terraform apply`
+	terraform.InitAndApply(t, terraformOptions)
+	// Run `terraform output` to get the value of an output variable
+	url := terraform.Output(t, terraformOptions, "ggz_instance_url")
+	// Verify that we get back a 200 OK with the expected text. It
+	// takes ~1 min for the Instance to boot, so retry a few times.
+	status := 200
+	text := "Hello, World!"
+	retries := 15
+	sleep := 5 * time.Second
+	http_helper.HttpGetWithRetry(t, url, status, text, retries, sleep)
+}
