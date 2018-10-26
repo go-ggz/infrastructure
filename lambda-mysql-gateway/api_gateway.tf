783554d6590320121b0f62a47fe643f92d359883
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-10-26 23:40:22 +0800

    add domain record.
    Signed-off-by: Bo-Yi Wu <appleboy.tw@gmail.com>

--- lambda-mysql-gateway/api_gateway.tf
+++ lambda-mysql-gateway/api_gateway.tf
@@ -5,3 +5,17 @@ resource "aws_api_gateway_domain_name" "demo" {
     types = ["REGIONAL"]
   }
 }
+
+# Route53 is not specifically required; any DNS host can be used.
+resource "aws_route53_record" "demo" {
+  zone_id = "${aws_route53_zone.dev.id}" # See aws_route53_zone for how to create this
+
+  name = "${aws_api_gateway_domain_name.demo.domain_name}"
+  type = "A"
+
+  alias {
+    name                   = "${aws_api_gateway_domain_name.demo.regional_domain_name}"
+    zone_id                = "${aws_api_gateway_domain_name.demo.regional_zone_id}"
+    evaluate_target_health = true
+  }
+}
