3f0d16ac907ffbc4ad23e43d37cc477f7f7bf80d
Bo-Yi Wu <appleboy.tw@gmail.com>
2018-10-27 00:09:38 +0800

    feat: add base path mapping in api gateway

--- lambda-mysql-gateway/api_gateway.tf
+++ lambda-mysql-gateway/api_gateway.tf
@@ -1,6 +1,7 @@
 resource "aws_api_gateway_domain_name" "demo" {
-  domain_name = "demo.dev.ggz.tw"
+  domain_name              = "demo.dev.ggz.tw"
   regional_certificate_arn = "${aws_acm_certificate.demo_cert.arn}"
+
   endpoint_configuration {
     types = ["REGIONAL"]
   }
@@ -18,4 +19,20 @@ resource "aws_route53_record" "demo" {
     zone_id                = "${aws_api_gateway_domain_name.demo.regional_zone_id}"
     evaluate_target_health = true
   }
+}
+
+data "aws_api_gateway_rest_api" "demo" {
+  name = "demo"
+}
+
+resource "aws_api_gateway_deployment" "demo" {
+  # See aws_api_gateway_rest_api_docs for how to create this
+  rest_api_id = "${data.aws_api_gateway_rest_api.demo.id}"
+  stage_name  = "staging"
+}
+
+resource "aws_api_gateway_base_path_mapping" "demo" {
+  api_id      = "${data.aws_api_gateway_rest_api.demo.id}"
+  stage_name  = "${aws_api_gateway_deployment.demo.stage_name}"
+  domain_name = "${aws_api_gateway_domain_name.demo.domain_name}"
 }
